02358ea3ef2915586c936073ccb4d298f8b80e25
David Hunnisett <david@inferiorplanet.co.uk>
2016-03-03 09:18:27 +0000

    working upto printing

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rm -rf data
 mkdir data
-
+cp -a ~/git/chai-client/static nginx
 cp -a ~/git/chai-client/static/assets/json/cards data/
 cp ~/.m2/repository/uk/co/elidirhealth/chai/chai/1.0-SNAPSHOT/chai-1.0-SNAPSHOT.jar data
 cp ~/git/chai-storage/chai/target/classes/config.yml  data
+cp -a ~/git/printing print
