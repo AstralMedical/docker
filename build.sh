5cb27f32b05af97ee06ef07692d0194d83bfb415
David Hunnisett <david@inferiorplanet.co.uk>
2016-09-07 15:06:20 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 rm -rf data
-mkdir data
+rm -rf print/printing
+rm -rf ngginx/static/*
+
+
+mkdir -p data/cards
 cp -a ~/git/chai-client/static nginx
-cp -a ~/git/chai-client/static/assets/json/cards data/
+cp -a ~/git/cards/static/* data/cards
+cp -a ~/git/cards/repeating/* data/cards
+cp -a ~/git/cards/careplans/* data/cards
 cp ~/.m2/repository/uk/co/elidirhealth/chai/chai/1.0-SNAPSHOT/chai-1.0-SNAPSHOT.jar data
 cp ~/git/chai-storage/chai/target/classes/config.yml  data
 cp -a ~/git/printing print
